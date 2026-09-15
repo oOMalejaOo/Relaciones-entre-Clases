04208ef786b6320ff15db846726adb06bb118c1e
oOMalejaOo <163355936+oOMalejaOo@users.noreply.github.com>
2026-09-15 08:47:31 -0500

    Delete Composicion.java

--- Composicion.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.composicion;
-
-public class Composicion {
-    public static void main(String[] args) {
-        Computadora miComputadora = new Computadora();
-        miComputadora.encender();
-    }
-}
