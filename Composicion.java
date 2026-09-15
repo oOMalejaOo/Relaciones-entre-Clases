a57edea433bc0b703e4fca154317884a92135c59
oOMalejaOo <163355936+oOMalejaOo@users.noreply.github.com>
2026-09-14 20:07:03 -0500

    Add files via upload

---
+++ Composicion.java
@@ -0,0 +1,8 @@
+package com.mycompany.composicion;
+
+public class Composicion {
+    public static void main(String[] args) {
+        Computadora miComputadora = new Computadora();
+        miComputadora.encender();
+    }
+}
